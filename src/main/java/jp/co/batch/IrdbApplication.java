e3e40328e92b55d06d3af5f478b4ffd529493996
kaikyou76 <kaikyou@gmail.com>
2023-06-20 10:08:35 +0900

    初期プロジェクト

---
+++ src/main/java/jp/co/batch/IrdbApplication.java
@@ -0,0 +1,13 @@
+package jp.co.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IrdbApplication.class, args);
+	}
+
+}
